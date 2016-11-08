2bc409828ea5fa7a504de83f4bb2c96f620f3fb1
marti733 <marti733@purdue.edu>
2016-11-08 07:44:54 -0500

    Add files via upload

---
+++ verilog/source/ann_controller.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   controller.sv
+// Created:     11/8/2016
+// Author:      Cheyenne Martinez
+// Lab Section: 337-03
+// Version:     1.0  Initial Design Entry
+// Description: ANN Controller
