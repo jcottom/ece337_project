3d90f12a85e72caa337910c9ecdff9cdc6de679e
marti733 <marti733@purdue.edu>
2016-11-08 07:50:30 -0500

    Update ram_controller.sv

--- verilog/source/ram_controller.sv
+++ verilog/source/ram_controller.sv
@@ -5,3 +5,12 @@
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
 // Description: RAM Controller
+
+module ram_controller
+  (
+    input wire start_detect,
+    input wire request,
+    output reg start_sram,
+    output reg image,
+    output reg image_load
+  );
