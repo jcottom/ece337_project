96718e8b3845fea2f7fbdd873ce237ec19a6d76b
marti733 <marti733@purdue.edu>
2016-11-08 07:42:14 -0500

    Update float_math.sv

--- verilog/source/float_math.sv
+++ verilog/source/float_math.sv
@@ -5,3 +5,10 @@
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
 // Description: Floating Math Timer
+
+module float_math
+  (
+    input wire start_math,
+    output reg math_done
+    output reg out
+  );
