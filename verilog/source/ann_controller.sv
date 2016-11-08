5913ffc6b5d33e82b939af2847becc8b6441153f
marti733 <marti733@purdue.edu>
2016-11-08 07:48:45 -0500

    Update ann_controller.sv

--- verilog/source/ann_controller.sv
+++ verilog/source/ann_controller.sv
@@ -5,3 +5,15 @@
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
 // Description: ANN Controller
+
+module ann_controller
+  (
+    input wire image_loaded,
+    input wire start,
+    output reg max_input,
+    output reg next_coeff,
+    output reg load_next,
+    output reg request,
+    output reg done,
+    output reg num_coeff
+  );
