ab111c835aa7092ff2daff5f8b6d46e51bf18bfc
marti733 <marti733@purdue.edu>
2016-11-08 07:40:55 -0500

    Update input.sv

--- verilog/source/input.sv
+++ verilog/source/input.sv
@@ -5,3 +5,15 @@
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
 // Description: Input to node timer
+
+module input
+  (
+    input wire max,
+    input wire next,
+    input wire math_done,
+    output reg done,
+    output reg start_math,
+    output reg input_num
+  );
+  
+  
