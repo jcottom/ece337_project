16d782bc510d7baa804aaf77aaf5355c498a18b7
marti733 <marti733@purdue.edu>
2016-11-20 09:49:51 -0500

    Update node.sv

--- verilog/source/node.sv
+++ verilog/source/node.sv
@@ -23,6 +23,8 @@ module node
 (
 	input wire clk,
 	input wire n_rst,
+	input wire start,
+	input wire reset_acc,
 	input wire [6:0] cnt_val,
 	input wire [15:0] coef [63:0],
 	input wire [15:0] data_in [63:0],
@@ -30,26 +32,42 @@ module node
 );
 
 //initialize internal variables
-	reg nxt_add; //next value to add to the accumulator
+	reg add1; //next value to add to the accumulator
+	reg add2; 
+	reg sum;
+	reg out;
 	reg accumulator; //stoes total sum of all the multiplication and addition
 
 //create instance of the activatin funciton
 
-always_ff @ (posedge clk, negedge n_rst)
-begin
-	if(n_rst == 0) begin
-		node_out = 0;
-		acculator = 0;
-	end  
-	else begin
-		fixed_point_add(.a(accumulator), .b(nxt_add), .result(accumulator)); //accumulator = accumulator + nxt_add
+	always_ff @ (posedge clk, negedge n_rst)
+	begin
+		if(n_rst == 0) begin
+			node_out = 0;
+			add2 = 0;
+		end  
+		else begin
+			// node_out = Activation functiion
+			add2 = out;
+		end
 	end
-end
+	
+	always @ (start, reset_acc) begin
+		if(reset_acc == 1) begin
+			out = 0;
+		end  
+		else if (start == 0) begin
+			out = sum;
+		end
+		else begin
+			out = add2;
+		end
+	end
 
-always_comb begin
-	fixed_point_mult(.a(coef[cnt_val]), .b(data_in[cnt_val]), .result(nxt_add)); //nxt_add = coef[cnt_val] * data_in[cnt_val];
-	node_out = activation_function(accumuulator);
-end
+	always_comb begin
+		fixed_point_mult(.a(coef[cnt_val]), .b(data_in[cnt_val]), .result(add1)); //add1 = coef[cnt_val] * data_in[cnt_val];
+		fixed_point_add(.a(add2), .b(add1), .result(sum)); //sum = add1 + add2
+	end
 
 
 endmodule
