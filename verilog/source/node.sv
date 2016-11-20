96706ec555ea27f2fef750c004ff935aad0f6491
marti733 <marti733@purdue.edu>
2016-11-20 09:26:53 -0500

    Update node

--- verilog/source/node.sv
+++ verilog/source/node.sv
@@ -16,36 +16,39 @@
 
 typedef reg [15:0] double;
 
-module node (
+module node 
+#(
+	localparam BITS = 16
+)
+(
 	input wire clk,
 	input wire n_rst,
 	input wire [6:0] cnt_val,
 	input wire [15:0] coef [63:0],
-	input wire [15:0] in_val [63:0],
+	input wire [15:0] data_in [63:0],
 	output wire [2:0] node_out
 );
 
 //initialize internal variables
-	//nxt_add: the next value to add to the accumulator 
-	//accumulator: stores the total sum of all of the mult and addition
+	reg nxt_add; //next value to add to the accumulator
+	reg accumulator; //stoes total sum of all the multiplication and addition
 
 //create instance of the activatin funciton
 
 always_ff @ (posedge clk, negedge n_rst)
 begin
 	if(n_rst == 0) begin
-		//reset node_out to 0
-		//reset the accumulator to 0
+		node_out = 0;
+		acculator = 0;
 	end  
 	else begin
-		//accumulator = accumulator + nxt_add
+		accumulator = accumulator + nxt_add;
 	end
 end
 
 always_comb begin
-	//nxt_add = coef[cnt_val] * in_val[cnt_val]
-
-	//node_out = activation_fucntion(accumuulator)
+	nxt_add = coef[cnt_val] * in_val[cnt_val];
+	node_out = activation_fucntion(accumuulator);
 end
 
 
