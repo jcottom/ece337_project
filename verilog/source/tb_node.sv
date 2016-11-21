03a824c7ae33813ed7b29b893a831243da9b9746
marti733 <marti733@purdue.edu>
2016-11-21 14:19:54 -0500

    Update tb_node.sv

--- verilog/source/tb_node.sv
+++ verilog/source/tb_node.sv
@@ -12,14 +12,18 @@
 module tb_node ();
 
 	localparam PERIOD = 10.0ns
+	localparam DELAY = 2.0ns
 
 	reg tb_clk;
 	reg tb_n_rst;
 	reg tb_start;
 	reg tb_reset_acc;
 	reg [6:0] tb_cnt_val [63:0];
+	reg [15:0] tb_coef [63:0];
 	reg [15:0] tb_data_in [63:0];
 	reg [2:0] tb_node_out;
+
+	integer i, j;
 
 	node DUT(.clk(tb_clk), .n_rst(tb_n_rst), .start(tb_start), .reset_acc(tb_reset_acc), .cnt_val(tb_cnt_val), .coef(tb_coef), .data_in(tb_data_in), .node_out(tb_node_out));
 
@@ -31,8 +35,23 @@ module tb_node ();
 	end 
 
 	initial begin
+
+		//Initialize random values for cnt_val, data_in, and coef
+		for (i = 0; i <= 6; i = i + 1) begin
+			tb_cnt_val[i] = {$random} * 2; // Only positive integers for cnt_val
+		end
 		
-		
+		for (j = 0; j <= 15; j = j + 1) begin
+			tb_data_in[j] = $random * 2;
+			tb_coef[j] = $random * 2;
+		end
+
+		//GOLD standard for determining expected output
+
+		//Tets n_rst
+		//Test Rest of accumulator
+
+
 	end
 
 endmodule
