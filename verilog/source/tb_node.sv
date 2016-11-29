e50e8a1476d812d2c026e8de431b3f5b3fe8c571
marti733 <marti733@purdue.edu>
2016-11-28 20:23:48 -0500

    Update tb_node.sv

--- verilog/source/tb_node.sv
+++ verilog/source/tb_node.sv
@@ -11,53 +11,95 @@
 
 module tb_node ();
 
-	localparam PERIOD = 10.0ns
-	localparam DELAY = 2.0ns
+	localparam PERIOD = 10.0ns;
+	localparam DELAY = 5.0ns;
 
 	reg tb_clk;
 	reg tb_n_rst;
 	reg tb_start;
 	reg tb_reset_acc;
-	reg [6:0] tb_cnt_val [63:0];
-	reg [15:0] tb_coef [63:0];
+	reg [6:0] tb_cnt_val;
+	reg [15:0] tb_coef [15:0];
 	reg [15:0] tb_data_in [63:0];
-	reg [2:0] tb_node_out;
+	reg [15:0] tb_node_out;
 
 	integer i, j;
 
-	node DUT(.clk(tb_clk), .n_rst(tb_n_rst), .start(tb_start), .reset_acc(tb_reset_acc), .cnt_val(tb_cnt_val), .coef(tb_coef), .data_in(tb_data_in), .node_out(tb_node_out));
-
 	always begin
 		tb_clk = 1'b0;
-		#(PERIOD / 2)
+		#(PERIOD / 2);
 		tb_clk = 1'b1;
-		#(PERIOD / 2)
+		#(PERIOD / 2);
 	end 
+
+	node DUT (.clk(tb_clk), .n_rst(tb_n_rst), .start(tb_start), .reset_acc(tb_reset_acc), .cnt_val(tb_cnt_val), .coef(tb_coef), .data_in(tb_data_in), .node_out(tb_node_out));
 
 	initial begin
 
-		//Initialize random values for cnt_val, data_in, and coef
-		for (i = 0; i <= 6; i = i + 1) begin
-			tb_cnt_val[i] = {$random} * 2; // Only positive integers for cnt_val
-		end
-		
-		for (j = 0; j <= 15; j = j + 1) begin
-			tb_data_in[j] = $random * 2;
-			tb_coef[j] = $random * 2;
-		end
+		//Initialize variables
+		tb_clk = 'b0;
+		tb_n_rst = 'b0;
+		tb_start = 'b0;
+		tb_reset_acc = 'b0;
+		tb_cnt_val = 'b0;
+		for (i = 0; i < 16; i = i + 1) begin
+			tb_coef[i] = 16'b0;
+		end 
+		for (j = 0; j < 64; j = j + 1) begin
+			tb_data_in[j] = 16'b0;
+		end 
 
-		//GOLD standard for determining expected output
+		#(PERIOD * 5);
 
-		//Tets n_rst
-		tb_n_rst = 0;
-
-		#(PERIOD * 2)
+		//TEST 0: n_rst
 
 		assert(tb_node_out == 0) 
-		else $error("Unable to reset node_out");
+			$info("Test 0: Complete");
+		else 
+			$error("Test 0: Unable to reset node_out");
 		
-		//Test Rest of accumulator
+		//TEST 1: Check node output
+		tb_cnt_val = 0;
+		tb_data_in[0] = 'b1;
+		tb_start = 0;
+		tb_reset_acc = 0;
 
+		#(PERIOD * 5);
+
+		assert(tb_node_out == 16'h0000)
+			$info("Test 1: Complete");
+		else 
+			$error("Test 1: INCORRECT node output");
+
+		#(DELAY);
+
+		//Test 2: Check start
+		tb_cnt_val = 1;
+		tb_data_in[tb_cnt_val] = 16'h0004;
+		tb_coef[tb_cnt_val] = 16'h0001;
+		tb_start = 1;
+		
+		#(PERIOD * 5);
+		
+		assert(tb_node_out == 16'h00A4)
+			$info("Test 2: Complete");
+		else 
+			$error("Test 2: INCORRECT output for start = 1");
+
+		//TEST 3: Check accumulator
+		#(DELAY);
+		tb_start = 0;
+		tb_cnt_val = 1;
+		tb_data_in[tb_cnt_val] = 16'h0004;
+		tb_coef[tb_cnt_val] = 16'h0001;
+
+		#(PERIOD * 5);
+		assert(tb_node_out == 16'h00A3)
+			$info("Test 3: Complete");
+		else 
+			$error("Test 3: INCORRECT output for accumulator");
+		
+		
 
 	end
 
