f49b1aa5ba97760ad4ab1f5290270cef5e18728a
marti733 <marti733@purdue.edu>
2016-12-04 20:09:09 -0500

    Update and rename tb_sram_ann to tb_sram_ann.sv

--- verilog/source/tb_sram_ann.sv
+++ verilog/source/tb_sram_ann.sv
@@ -15,9 +15,9 @@ module tb_sram_ann ();
 	reg tb_p_done;
 	reg [31:0] tb_read_data;
 	reg tb_start_address;
-	reg tb_address;
+	reg [9:0] tb_address;
 	reg tb_read;
 
 	sram_ann (.clk(tb_clk), .n_rst(tb_n_rst), .coef_select(tb_coef_select), .i_loaded(tb_i_loaded), .p_done(tb_p_done), .read_date(tb_read_data), start_address(tb_start_address), .address(tb_address), .read(tb_read));
-
+	
 endmodule 
