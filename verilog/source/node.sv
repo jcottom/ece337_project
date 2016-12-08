d97bdc10c378cf534d8e895eaa8b99efa6bc5e92
marti733 <marti733@purdue.edu>
2016-12-07 20:16:16 -0500

    Comment and clean up code

--- verilog/source/node.sv
+++ verilog/source/node.sv
@@ -1,7 +1,7 @@
 // $Id: $
 // File name:   node.sv
 // Created:     11/01/2016
-// Author:      Ryan McBee
+// Author:      Ryan McBee, Cheyenne Martinez, and Taylor Lipson
 // Lab Section: 7
 // Version:     1.0  Initial Design Entry
 // Description: Artificial neural network individual node
@@ -44,7 +44,6 @@ module node
 	
 
 //create instance of the activatin funciton
-
 	always_ff @(posedge clk, negedge n_rst)
 	begin
 		if(n_rst == 0) begin
@@ -63,7 +62,7 @@ module node
 		end
 	end
 	
-	always_comb begin
+	always_comb begin // Next out based on current inputs
 		if(reset_acc == 1) begin
 			nxt_out = 0;
 		end  
@@ -76,9 +75,8 @@ module node
 	end
 
 	fixed_point_mult mult(.a(coef[cnt_val]), .b(data_in[cnt_val]), .result(add1));
-	//fixed_point_mult mult(.a(coef[IMAGE_SIZE - 1 - cnt_val]), .b(data_in[IAMGE_SIZE - 1 - cnt_val]), .result(add1));
 	fixed_point_add  add(.a(add2), .b(add1), .result(sum)); //sum = add1 + add2
-	new_activation       active(.in(add2[31:16]) ,.out(node_out));
+	new_activation       active(.in(add2[31:16]) ,.out(node_out)); //Use only top 16 bits
 
 
 endmodule
